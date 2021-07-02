6b6ddd9108774f03a3cb03dbbc0c673bbb89b460
5elenay <5elenay@protonmail.com>
2021-07-02 17:16:46 +0300

    Added Zip Command.

--- handler.go
+++ handler.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"bytes"
+	"compress/zlib"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
+	"time"
 )
+
+type DelfinData struct {
+	data         []byte
+	path         string
+	is_directory bool
+}
 
 var allParameters = map[string][2]string{
 	"help":  {"Shows list of all commands.", "delfin help | delfin help <parameter>"},
@@ -27,6 +39,9 @@ func HandleArguments() {
 	case "help":
 		HandleHelp(parameters[1:])
 		break
+	case "zip":
+		HandleZip(parameters[1:])
+		break
 	default:
 		fmt.Println(parameters)
 	}
@@ -45,7 +60,63 @@ func HandleHelp(params []string) {
 		if value, status := allParameters[strings.ToLower(param)]; status {
 			fmt.Printf("Description: %s\nUsage: %s\n", value[0], value[1])
 		} else {
-			fmt.Println("Parameter not found!")
+			log.Fatal("Parameter not found!")
+			os.Exit(2)
+		}
+	}
+}
+
+func HandleZip(params []string) {
+	if len(params) < 2 {
+		log.Fatal("Please provide input and output location.")
+		os.Exit(2)
+	} else {
+		input, output := params[0], params[1]
+
+		if CheckDirectory(input) && CheckDirectory(output) {
+			var all_files []string
+			folder_splitted := strings.Split(output, "/")
+			file_name := folder_splitted[len(folder_splitted)-1]
+
+			if file_name == "" {
+				file_name = fmt.Sprintf("compressed_%s", time.Now().Format("2006-01-02_15-04-05"))
+			}
+
+			fmt.Println(file_name)
+
+			err := filepath.Walk(input, func(path string, info fs.FileInfo, err error) error {
+				if CheckDirectory(path) == false {
+					var input bytes.Buffer
+
+					data, readError := ioutil.ReadFile(path)
+
+					if readError != nil {
+						log.Fatal("Unexcepted Error", readError)
+						os.Exit(4)
+					}
+
+					writer := zlib.NewWriter(&input)
+					writer.Write(data)
+					writer.Close()
+
+					formatted := fmt.Sprintf("%s:%d:%s", path, 0, input.String())
+					all_files = append(all_files, formatted)
+				} else {
+					formatted := fmt.Sprintf("%s:%d:%s", path, 1, "-")
+					all_files = append(all_files, formatted)
+				}
+				return err
+			})
+
+			if err != nil {
+				log.Fatal("Unexcepted Error", err)
+				os.Exit(4)
+			}
+
+			os.WriteFile(fmt.Sprintf("%s/%s.delfin", output, file_name), []byte(strings.Join(all_files, "\n")), 0666)
+		} else {
+			log.Fatal("Input or output location is not found / is not a directory.")
+			os.Exit(3)
 		}
 	}
 }
